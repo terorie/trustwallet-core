e81ede08e4574a7b28c599627adaff6ddb2fd94a
Richard Patel <richxpatel@gmail.com>
2019-03-04 17:33:23 +0100

    Nimiq C++ implementation

---
+++ tests/Nimiq/SignerTests.cpp
@@ -0,0 +1,44 @@
+// Copyright © 2017-2019 Trust.
+//
+// This file is part of Trust. The full Trust copyright notice, including
+// terms governing use, modification, and redistribution, is contained in the
+// file LICENSE at the root of the source code distribution tree.
+
+#include "HexCoding.h"
+#include "Nimiq/Address.h"
+#include "Nimiq/EdPrivateKey.h"
+#include "Nimiq/Signer.h"
+#include "Nimiq/Transaction.h"
+
+#include <gtest/gtest.h>
+
+namespace TW {
+namespace Nimiq {
+
+TEST(NimiqSigner, DerivePublicKey) {
+    const EdPrivateKey privateKey(parse_hex("e3cc33575834add098f8487123cd4bca543ee859b3e8cfe624e7e6a97202b756"));
+    const auto publicKey = privateKey.getPublicKey();
+    const auto address = Address(publicKey);
+    ASSERT_EQ(address.string(), "NQ27 GBAY EVHP HK5X 6JHV JGFJ 5M3H BF4Y G7GD");
+}
+
+TEST(NimiqSigner, Sign) {
+    const EdPrivateKey privateKey(parse_hex("e3cc33575834add098f8487123cd4bca543ee859b3e8cfe624e7e6a97202b756"));
+    const auto publicKey = privateKey.getPublicKey();
+
+    Transaction tx(
+        publicKey,
+        Address("NQ86 2H8F YGU5 RM77 QSN9 LYLH C56A CYYR 0MLA"),
+        42042042,
+        1000,
+        314159
+    );
+
+    Signer signer;
+    signer.sign(privateKey, tx);
+    
+    ASSERT_EQ(hex(tx.signature),
+        "74dc7f6e0ab58a0bf52cc6e8801b0cca132dd4229d9a3e3a3d2f90e4d8f045d981b771bf5fc3851a98f3c617b1a943228f963e910e061808a721cfa0e3cad50b");
+}
+
+}} // namespace
